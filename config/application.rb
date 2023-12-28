e5fa88494fb70cba0782efa36824bd56a694ce51
kosui3625 <154409416+kosui3625@users.noreply.github.com>
2023-12-28 16:42:53 +0900

    チャットルームの削除機能の実装

--- config/application.rb
+++ config/application.rb
@@ -9,9 +9,9 @@ Bundler.require(*Rails.groups)
 module ChatApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.0
-
-    # Configuration for the application, engines, and railties goes here.
+    config.load_defaults 7.0 
+    config.i18n.default_locale = :ja
+    config.time_zone = 'Tokyo' # engines and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
